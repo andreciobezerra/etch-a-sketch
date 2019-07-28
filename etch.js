2e861e65748c3931d256c578c5a1c99c749899d9
andrecio <andrecio@ufrn.edu.br>
2019-07-28 01:08:35 -0300

    Version 1.0.0

--- etch.js
+++ etch.js
@@ -7,15 +7,48 @@ const colorInput = document.querySelector('#color-input')
 
 //Events
 
+btnClean.onclick = clear
+btnResize.onclick = resize
+btnRandom.onclick = colorRandom
 colorInput.onchange = colorFixed
 
 //Functions
-function colorFixed(){
-  const blocks=document.querySelectorAll('.block')
-  blocks.forEach(block => block.onmouseenter = () => block.style.background = colorInput.value)
+function clear(){
+  let blocks = document.querySelectorAll('.block')
+  blocks.forEach(block => block.style.background = '#d0d0d0')  
 }
 
-const addBlock = (amount, element) => {
+const catchSize = () => Number(prompt('Enter the new amount of blocks per side(above>0):'))
+
+function resize(){
+  newSize = catchSize()
+  if(newSize<1) resize()
+  const currentGrid = document.querySelector('#sketch')
+  currentGrid.parentNode.removeChild(currentGrid)
+  createGrid(newSize)
+}
+
+function colorRandom(){
+  const blocks = document.querySelectorAll('.block')
+  blocks.forEach(block=>{
+    let r = Math.floor(Math.random()*255)
+    let g = Math.floor(Math.random()*255)
+    let b = Math.floor(Math.random()*255)
+    console.log(`rgb(${r},${g},${b})`);
+    
+    block.removeEventListener('mouseenter',colorFixed)
+    block.onmouseenter = () => block.style.background = `rgb(${r},${g},${b})`
+  }) 
+}
+
+function colorFixed(){
+  const blocks=document.querySelectorAll('.block')
+  blocks.forEach(block => {
+    block.removeEventListener('mouseenter',colorRandom)
+    block.onmouseenter = () => block.style.background = colorInput.value})
+}
+
+function addBlock(amount, element){
   const block=document.createElement('div')
   block.classList.add('block')
   if(amount===0) return 
@@ -24,11 +57,12 @@ const addBlock = (amount, element) => {
   addBlock(amount-1,element)
 }
 
-const createGrid = (size=16) =>{
+function createGrid(size=16){
   const grid = document.createElement('div')
   grid.setAttribute('id','sketch')
   container.appendChild(grid)
-  addBlock(size**2,grid)
+  grid.style.setProperty('--setGrid',size)
+  addBlock(size*size,grid)
 }
 
 //Call
